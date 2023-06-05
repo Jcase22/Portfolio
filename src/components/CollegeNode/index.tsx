8658a7bad2a3ede61f7400e596c0ed07ac1e04f6
Justin Case <justin.case1061@gmail.com>
2023-06-05 14:46:11 -0500

    final touches on styling, removed server folder

--- src/components/CollegeNode/index.tsx
+++ src/components/CollegeNode/index.tsx
@@ -6,10 +6,10 @@ const CollegeNode = () => {
     <div className='node-container'>
       <div className='content-container'>
       <div className='node-header-container'>
-        <h2 id='company-name'>The University of Texas at Dallas</h2>
-        <span id='location'>Richardson, TX</span>
+        <h2 id='college-name'>The University of Texas at Dallas</h2>
+        <span id='college-location'>Richardson, TX</span>
       </div>
-      <h2 id='summary'>Bachelors of Science in Finance</h2>
+      <h2 id='college-major'>Bachelors of Science in Finance</h2>
       </div>
     </div>
   )
--- src/components/ContactForm/index.tsx
+++ src/components/ContactForm/index.tsx
@@ -21,16 +21,16 @@ const ContactForm = () => {
     <div>
       <div className='section-title-container'>
         <h1 className='section-title'>Contact</h1>
-        <h4 className='section-subtitle'>Want to reach out?  Fill out the form below or contact me via my any of my social media platforms</h4>
+        <h4 className='section-subtitle'>Want to reach out?  Fill out the form below or contact me via any of my social media platforms</h4>
       </div>
 
       <form onSubmit={handleSubmit} className='form-container'>
 
-        <input type="email" value={email} placeholder="Email" name="email" onChange={(e) => {setEmail(e.target.value)}} className='input-bar' />
+        <input type="email" value={email} placeholder="Email" name="email" onChange={(e) => { setEmail(e.target.value) }} className='input-bar' />
 
-        <input type="text" value={subject} placeholder="Subject" name="subject" onChange={(e) => {setSubject(e.target.value)}} className='input-bar' />
+        <input type="text" value={subject} placeholder="Subject" name="subject" onChange={(e) => { setSubject(e.target.value) }} className='input-bar' />
 
-        <textarea value={message} placeholder="Message" name="message" onChange={(e) => {setMessage(e.target.value)}} className='message-box' />
+        <textarea value={message} placeholder="Message" name="message" onChange={(e) => { setMessage(e.target.value) }} className='message-box' />
 
         <div className='submit-container'>
           <button type="submit" className='submit-button'>Submit</button>
