d40aff38052f8b69f857a5bf90d2442e4c068747
Justin Case <justin.case1061@gmail.com>
2023-06-08 22:34:25 -0500

    added margin right to logo components and fixed typo

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -12,7 +12,7 @@ const About = () => {
 
           <div className='body-paragraph'>My passion for web development led me to enroll in Hack Reactor, the nations leading coding boot camp, where I learned the ins and out of web development and honed the finer points of my craft.  During my time at Hack Reactor I worked with React, Redux, and Node.js.  These technologies gave me the resources needed to build full-scale modern websites with working back-end components.</div>
 
-          <div className='body-paragraph'>Soon after, I began my career as a full-stack software engineer with NomiSo, a software consulting firm.  There, I built clean interactive web applications using React, Typescript, HTML, and CSS.  During my time with NomiSo I picked up React Native, allowing me move into mobile development.</div>
+          <div className='body-paragraph'>Soon after, I began my career as a full-stack software engineer with NomiSo, a software consulting firm.  There, I built clean interactive web applications using React, Typescript, HTML, and CSS.  During my time with NomiSo I picked up React Native, allowing me to move into mobile development.</div>
 
           <div className='body-paragraph'>Outside of work I enjoy working on personal projects, working out, spending quality time with my friends and family, and exploring the city of Chicago.  I am currently looking for new opportunities and would love to hear from you!</div>
 
