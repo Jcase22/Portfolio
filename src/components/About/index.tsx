ba9de8ea9402885c2d37d0317192d59904af7c9e
Justin Case <justin.case1061@gmail.com>
2023-11-08 15:56:42 -0600

    changed for seo

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -6,17 +6,17 @@ const About = () => {
   return (
     <div>
       <div className="about-container">
-        <div className='body-container'>
+        <body className='body-container'>
 
-          <div className='body-paragraph'>I am a Vietnamese-American Software Engineer with a passion for all things code related.  My specific interest in web development started during COVID. Despite being a finance major during my undergraduate studies, I took on coding as a way to destress and quickly found myself practicing more and more. Over the course of several months I taught myself Javascript, HTML, and CSS. Then, I used those skills to build my own responsive websites. What originated as a hobby to pass the time eventually developed into a newfound passion that I decided to change the course of my professional career to pursue.</div>
+          <p className='body-paragraph'>I am a Vietnamese-American Software Engineer with a passion for all things code related.  My specific interest in web development started during COVID. Despite being a finance major during my undergraduate studies, I took on coding as a way to destress and quickly found myself practicing more and more. Over the course of several months I taught myself Javascript, HTML, and CSS. Then, I used those skills to build my own responsive websites. What originated as a hobby to pass the time eventually developed into a newfound passion that I decided to change the course of my professional career to pursue.</p>
 
-          <div className='body-paragraph'>My passion for web development and being displaced from college led me to enroll in Hack Reactor, the nations leading coding boot camp, where I learned the ins and out of web development and honed the finer points of my craft.  During my time at Hack Reactor I worked with React, Redux, and Node.js.  These technologies gave me the resources needed to build full-scale modern websites with working back-end components.</div>
+          <p className='body-paragraph'>My passion for web development and being displaced from college led me to enroll in Hack Reactor, the nations leading coding boot camp, where I learned the ins and out of web development and honed the finer points of my craft.  During my time at Hack Reactor I worked with React, Redux, and Node.js.  These technologies gave me the resources needed to build full-scale modern websites with working back-end components.</p>
 
-          <div className='body-paragraph'>Soon after, I began my career as a full-stack software engineer with NomiSo, a software consulting firm.  There, I built clean interactive web applications using React, Typescript, HTML, and CSS.  During my time with NomiSo I picked up React Native, allowing me to move into mobile development.</div>
+          <p className='body-paragraph'>Soon after, I began my career as a full-stack software engineer with NomiSo, a software consulting firm.  There, I built clean interactive web applications using React, Typescript, HTML, and CSS.  During my time with NomiSo I picked up React Native, allowing me to move into mobile development.</p>
 
-          <div className='body-paragraph'>Outside of work I enjoy working on personal projects, working out, spending quality time with my friends and family, and exploring the city of Chicago.  I am currently looking for new opportunities and would love to hear from you!</div>
+          <p className='body-paragraph'>Outside of work I enjoy working on personal projects, working out, spending quality time with my friends and family, and exploring the city of Chicago.  I am currently looking for new opportunities and would love to hear from you!</p>
 
-        </div>
+        </body>
         <div className='image-container'>
           <img src={selfie} className='image' />
         </div>
--- src/components/HomeButton/index.tsx
+++ src/components/HomeButton/index.tsx
@@ -30,7 +30,7 @@ const HomeButton = ({ buttonText }: HomeButtonProps) => {
   return (
     <div className='home-button-container fade-in' onClick={clickBehavior()}>
       <div className='home-button-text'>{buttonText}</div>
-        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="14" className="arrow"><path fill="none" fill-rule="evenodd" stroke="#ffffff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 7h16M11 1l6 6-6 6"></path></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="14" className="arrow"><path fill="none" fillRule="evenodd" stroke="#ffffff" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 7h16M11 1l6 6-6 6"></path></svg>
     </div>
   )
 }
